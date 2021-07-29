4c728e2ee8ce8052ba0c9eb3051d18848ff83697
Mmariami <mxarshiladze29@gmail.com>
2021-07-29 15:56:42 +0300

    task2 complete

---
+++ task02/script.js
@@ -0,0 +1,12 @@
+const calculateEngravingPrice = function (message, pricePerWord) {
+    let splitArr = sentence.split(" ");
+    message = splitArr.length
+
+    return message * pricePerWord
+}
+
+
+let sentence = 'Mariam will become front-end Developer'
+console.log (calculateEngravingPrice(sentence,10));
+
+// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis',20));
